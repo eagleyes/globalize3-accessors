74ea4fb5e6d9be468176abc4366119a5aca91fa4
eagleyes <eagleyes12@gmail.com>
2011-04-12 21:26:19 -0400

    added localized methods to each translatable symbols

--- lib/globalize3_accessors.rb
+++ lib/globalize3_accessors.rb
@@ -1,28 +1,31 @@
-module Globalize3Accessors
-	def self.included(base)
-		base.extend InstanceMethods
-	end
-	module InstanceMethods
-		def easy_translate options = {}
-			options[:locales].each do |this_locale|
-			
-				globalize_options[:translated_attributes].each do |this_method|
-			
-					class_eval(%|
-						def #{this_locale}_#{this_method}; 
-							globalize.fetch("#{this_locale}","#{this_method}");
-						end;
-					|)
-			
-					class_eval("def #{this_locale}_#{this_method}=(this_value);" +
-						%|
-							globalize.stash("#{this_locale}","#{this_method}", this_value);
-						end;
-						|)
-				end unless globalize_options[:translated_attributes].empty?
-			
-			end if options[:locales]
+module ActiveRecord
+  module Globalize3Accessors
+    def self.included(base)
+      base.extend Accessors
+    end
 
-		end
-	end 
+    module Accessors
+
+			def create_accessors
+        #puts globalize.translated_attribute_names
+				
+				I18n.available_locales.each do |locale|
+					this_locale = locale.to_s.gsub('-','_')
+        		self.translated_attribute_names.each do |this_method|
+
+          	define_method :"#{this_method}_#{this_locale}" do
+          	  globalize.fetch "#{this_locale}", this_method
+          	end
+
+          	define_method("#{this_method}_#{this_locale}=") do |val|
+          	  globalize.write("#{this_locale}","#{this_method}","#{val}")
+          	end
+
+        		end unless translated_attribute_names.empty?
+				end
+			end
+    end
+  end
 end
+
+ActiveRecord::Base.send :include, ActiveRecord::Globalize3Accessors
